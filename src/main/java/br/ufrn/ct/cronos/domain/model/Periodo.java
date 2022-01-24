15972e704f90ee424e655eaa64cd07ce378a56a2
Erick Sharlls <sharlls@MacBookAir.local>
2022-01-24 16:24:21 -0300

    Removendo anotações do bean validations da entidade de domínio Periodo

--- src/main/java/br/ufrn/ct/cronos/domain/model/Periodo.java
+++ src/main/java/br/ufrn/ct/cronos/domain/model/Periodo.java
@@ -9,16 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import javax.validation.constraints.NotNull;
-
-import br.ufrn.ct.cronos.core.validations.IntervaloEntreDatas;
-import br.ufrn.ct.cronos.core.validations.NotBlankAndSizeForString;
-import br.ufrn.ct.cronos.core.validations.NotNullAndRangeForNumber;
-import br.ufrn.ct.cronos.core.validations.RangeForYear;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@IntervaloEntreDatas(dataInicialField = "dataInicio", dataFinalField = "dataTermino")
 @Data
 @EqualsAndHashCode (onlyExplicitlyIncluded = true)
 @Entity
@@ -31,30 +24,24 @@ public class Periodo {
 	@Column(name = "id_periodo")
 	private Long id;
 	
-	@NotBlankAndSizeForString(max = 30)
 	@Column(name="nome_periodo")
 	private String nome;
 	
-	@NotBlankAndSizeForString(max = 50)
 	@Column(name="descricao_periodo")
 	private String descricao;
 	
-	@NotNull
 	@Column(name="data_inicio_periodo")
 	private LocalDate dataInicio;
 	
-	@NotNull
 	@Column(name="data_termino_periodo")
 	private LocalDate dataTermino;
 	
 	@Column(name="is_periodo_letivo")
 	private Boolean isPeriodoLetivo;
 	
-	@RangeForYear
 	@Column(name="ano_periodo")
 	private Short ano;
 
-	@NotNullAndRangeForNumber(min = 1, max = 10)
 	@Column(name="numero_periodo")
 	private Short periodo;
 	
