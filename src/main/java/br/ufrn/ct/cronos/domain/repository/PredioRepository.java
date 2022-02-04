9a1461ad0494e78498f457e00ac6c069b1bdf423
Erick Sharlls <sharlls@MacBookAir.local>
2022-02-04 13:43:25 -0300

    Modificações no Repositório de Prédio

--- src/main/java/br/ufrn/ct/cronos/domain/repository/PredioRepository.java
+++ src/main/java/br/ufrn/ct/cronos/domain/repository/PredioRepository.java
@@ -1,6 +1,7 @@
 package br.ufrn.ct.cronos.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
