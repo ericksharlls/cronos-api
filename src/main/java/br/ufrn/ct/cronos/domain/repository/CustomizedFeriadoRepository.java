38b990964c1296cd1b9c7effa846c5139ed1de46
MuriloHD <muriloddd1@gmail.com>
2022-03-07 18:23:58 -0300

    Alterações no FeriadoRepository

--- src/main/java/br/ufrn/ct/cronos/domain/repository/CustomizedFeriadoRepository.java
+++ src/main/java/br/ufrn/ct/cronos/domain/repository/CustomizedFeriadoRepository.java
@@ -2,9 +2,7 @@ package br.ufrn.ct.cronos.domain.repository;
 
 
 import java.time.LocalDate;
-import java.util.List;
 
-import br.ufrn.ct.cronos.domain.model.Feriado;
 
 
 
@@ -13,10 +11,5 @@ public interface CustomizedFeriadoRepository {
 
 	Boolean verificarSeADataDoFeriadoCorrespondeAoPeriodoInformado(Long periodoId,LocalDate data);
 	
-	Boolean verificarSeJaHaUmFeriadoComMesmaData(LocalDate data);
 	
-	List<Feriado> consultarPorPeriodoId(Long periodoId);
-	
-	
-
 }
--- src/main/java/br/ufrn/ct/cronos/domain/repository/FeriadoRepository.java
+++ src/main/java/br/ufrn/ct/cronos/domain/repository/FeriadoRepository.java
@@ -1,7 +1,20 @@
 package br.ufrn.ct.cronos.domain.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import br.ufrn.ct.cronos.domain.model.Feriado;
 
 public interface FeriadoRepository extends CustomJpaRepository<Feriado, Long>, CustomizedFeriadoRepository {
+	
+	@Query("from Feriado where periodo.id like %:periodoId%")
+	List<Feriado> findByPeriodo(@Param("periodoId") Long periodoId);
 
+	Optional<Feriado> findByData(LocalDate data);
 }
+
+
