a8fe8044212a22b20b40932c328b75baca1ee3ca
Guilherme Bezerra <guilherme.bezerra@esig.com.br>
2022-06-02 12:08:06 -0300

    Foi realizado a implementação do endpoint bem como os teste de paginação

---
+++ src/main/java/br/ufrn/ct/cronos/api/controller/DepartamentoController.java
@@ -0,0 +1,40 @@
+package br.ufrn.ct.cronos.api.controller;
+
+import br.ufrn.ct.cronos.api.assembler.DepartamentoModelAssembler;
+import br.ufrn.ct.cronos.api.model.FeriadoModel;
+import br.ufrn.ct.cronos.api.model.FuncionarioModel;
+import br.ufrn.ct.cronos.api.model.input.DepartamentoModel;
+import br.ufrn.ct.cronos.domain.model.Departamento;
+import br.ufrn.ct.cronos.domain.model.Feriado;
+import br.ufrn.ct.cronos.domain.repository.DepartamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/departamentos")
+public class DepartamentoController {
+    @Autowired
+    DepartamentoRepository departamentoRepository;
+
+    @Autowired
+    DepartamentoModelAssembler departamentoModelAssembler;
+
+    @GetMapping
+    public Page<DepartamentoModel> buscar (@PageableDefault(size = 10) Pageable pageable) {
+        Page<Departamento> departamentosPage = departamentoRepository.findAll(pageable);
+
+        Page<DepartamentoModel> departamentoModelPage = new PageImpl<>(
+                departamentoModelAssembler.toCollectionModel(departamentosPage.getContent()),
+                                                                                    pageable,
+                                                                                    departamentosPage.getTotalElements()
+        );
+
+        return departamentoModelPage;
+    }
+}
---
+++ src/main/java/br/ufrn/ct/cronos/api/model/input/DepartamentoModel.java
@@ -0,0 +1,13 @@
+package br.ufrn.ct.cronos.api.model.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DepartamentoModel {
+
+    private String nome;
+
+    private  String descricao;
+}
